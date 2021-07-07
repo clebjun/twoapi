28360c3c340a01c6bd01e995c9b7d12539ea6698
clebjun <acjunior4@stefanini.com>
2021-07-07 02:19:46 -0300

    Projeto de testes

---
+++ NUnitIntegrationTest/API2ControllerTest.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using TwoAPI;
+
+namespace APITest
+{
+    [TestFixture]
+    public class API2ControllerTest
+    {
+
+        [Test]
+        public void ReturnCorrectCode()
+        {
+            var api2Controller = new TwoAPI.Controllers.API2Controller();
+            var result = api2Controller.ShowMeTheCode();
+
+            Assert.AreEqual("https://github.com/clebjun/twoapi.git", result);
+        }
+
+        [Test]
+        [TestCase(100, 5, "105,10")]
+        [TestCase(1000, 10, "1104,62")] 
+        [TestCase(500, 20, "610,10")] 
+        public void CalcularJuros(double valorInicial, int meses, string resultadoEsperado)
+        {
+            var api2Controller = new TwoAPI.Controllers.API2Controller();
+            var result = api2Controller.GetCalculoJuros(valorInicial, meses);
+
+            Assert.AreEqual(resultadoEsperado, result.ToString());
+        }
+    }
+}
---
+++ TwoAPI/API1ControllerTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace APITest
+{
+    [TestFixture]
+    public class API1ControllerTest
+    {
+        [Test]
+        public void RetornoTaxaJuros()
+        {
+            var api1Controller = new TwoAPI.Controllers.API1Controller();
+            var result = api1Controller.GetTaxaJuros();
+
+            Assert.AreEqual(result, "0,01");
+        }
+
+    }
+}
